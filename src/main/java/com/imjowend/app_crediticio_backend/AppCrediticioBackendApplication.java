f9a348c5eacdc525af9ea3548b5ec3523c480045
imjowend <joaquinvcestudio@gmail.com>
2024-06-23 19:53:22 -0500

    initial commit

---
+++ src/main/java/com/imjowend/app_crediticio_backend/AppCrediticioBackendApplication.java
@@ -0,0 +1,13 @@
+package com.imjowend.app_crediticio_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCrediticioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppCrediticioBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imjowend/app_crediticio_backend/AppCrediticioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imjowend.app_crediticio_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCrediticioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
